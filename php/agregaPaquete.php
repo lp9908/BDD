c602e192a34cc9179d9ce9cc98cad932430fa36a
Marín Domínguez <trance9908@hotmail.com>
2016-10-29 17:55:22 -0500

    Correccion error agregarPaquete

--- php/agregaPaquete.php
+++ php/agregaPaquete.php
@@ -3,13 +3,12 @@
 	$idPaquete = $_POST["idPaquete"];
 	$nombrePaquete = $_POST["nombrePaquete"];
 	$Precio = $_POST["Precio"];
-	$estado = 1
-	try {
+	$estado = 1;
+	try{
     	$sql = "INSERT INTO paquete VALUES ($idPaquete,'$nombrePaquete',$Precio,$estado)";
     	$conn->exec($sql);
     	echo "Paquete agregado";
-    }
-	catch(PDOException $e){
+    }catch(PDOException $e){
     	echo $sql . "<br>" . $e->getMessage();
     }
 	$conn = null;
--- php/agregaProducto.php
+++ php/agregaProducto.php
@@ -5,12 +5,11 @@
 	$Precio = $_POST["Precio"];
 	$existencia = $_POST["existencia"];
 	$estado = 1;
-	try {
+	try{
     	$sql = "INSERT INTO producto VALUES ($idProducto,'$nombreProducto', $Precio,$existencia, $estado)";
     	$conn->exec($sql);
     	echo "Producto agregado";
-    }
-	catch(PDOException $e){
+    }catch(PDOException $e){
     	echo $sql . "<br>" . $e->getMessage();
     }
 	$conn = null;
