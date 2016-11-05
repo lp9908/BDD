5196530c4574f4665caa7a8b2ce66dfcb7aa444e
enathergar <enataliahg@hotmail.com>
2016-11-05 14:28:14 -0600

    Punto de Venta Boletos

--- php/listar_peliculas.php
+++ php/listar_peliculas.php
@@ -1,13 +1,19 @@
 <?php
-  function listar_peliculas(){
-    include "conexion_servidor.php";
-    $tables = "<option value=''> Selecciona pelicula</option>";
-    $sql = "SELECT idpelicula, nombre FROM pelicula WHERE estado = 1";
-    foreach ($conn->query($sql) as $row){
-		$tables .= "<option value='$row[0]'>".$row[1]."</option>";
+    function listarPeliculas(){
+        include 'conexion_BD.php';
+        $lista_peliculas = '<option value="">Selecciona pelicula</option>';
+        try{
+            $stmt = $conn->prepare("SELECT idpelicula, nombre FROM pelicula order by nombre");
+            $stmt->execute();
+            $peliculas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($peliculas as $pelicula){
+                $lista_peliculas .= '<option value="'.$pelicula['idpelicula'].'">'.$pelicula['nombre'].'</option>';
+            }
+            echo $lista_peliculas;
+        }
+        catch (PDOException $e){
+            echo $e->getMessage();
+        }
     }
-    echo $tables;
-    $conn = null;
-  }
-  listar_peliculas();
+    listarPeliculas();
 ?>
